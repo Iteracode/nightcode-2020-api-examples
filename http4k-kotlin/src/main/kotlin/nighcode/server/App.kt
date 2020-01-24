c12ff26c19e7d3fb12f1b633db45fa5d21261cc6
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2020-01-24 19:13:06 +0100

    Manage 'name' from query and body

--- http4k-kotlin/src/main/kotlin/nighcode/server/App.kt
+++ http4k-kotlin/src/main/kotlin/nighcode/server/App.kt
@@ -1,6 +1,7 @@
 package nighcode.server
 
 import org.http4k.core.Body
+import org.http4k.core.ContentType
 import org.http4k.core.Method.DELETE
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
@@ -13,17 +14,32 @@ import org.http4k.server.asServer
 import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.format.Jackson.auto
+import org.http4k.format.Jackson.json
 import org.http4k.core.with
+import org.http4k.lens.nonEmptyString
 
+data class Name(val name: String?)
 data class Greetings(val message: String)
 
 fun main(args: Array<String>) {
+    val nameLens = Body.auto<Name>().toLens()
     val greetingsLens = Body.auto<Greetings>().toLens()
 
     // val app = { request: Request -> Response(OK).body("Nightcode") }
     val app = routes(
         "/" bind GET to { Response(OK).body("Nightcode") },
-        "/api/greetings" bind GET to { Response(OK).with(greetingsLens of Greetings("hello, world")) }
+        "/api" bind routes(
+            "/greetings" bind routes(
+                GET to { request: Request -> 
+                    val name: String? = request.query("name")
+                    Response(OK).with(greetingsLens of Greetings("hello, ${name ?: "world"}"))
+                },
+                POST to { request: Request -> 
+                    val name: Name = nameLens.extract(request)
+                    Response(OK).with(greetingsLens of Greetings("hello, ${name.name ?: "world"}"))
+                }
+            )
+        )
     )
 
     app.asServer(Jetty(3000)).start()
