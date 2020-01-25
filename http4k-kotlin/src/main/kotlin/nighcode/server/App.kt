f6b5f57d8579561c6ba740c564fab286bc0dd1a4
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2020-01-25 17:41:34 +0100

    HTTP4K + Kotlin - Manage 400 from query

--- http4k-kotlin/src/main/kotlin/nighcode/server/App.kt
+++ http4k-kotlin/src/main/kotlin/nighcode/server/App.kt
@@ -1,38 +1,33 @@
 package nighcode.server
 
-import org.http4k.core.Body
-import org.http4k.core.ContentType
-import org.http4k.core.Method.DELETE
+import org.http4k.core.*
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
-import org.http4k.core.Method
-import org.http4k.core.Request
-import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
-import org.http4k.server.Jetty
-import org.http4k.server.asServer
+import org.http4k.filter.ServerFilters
+import org.http4k.format.Jackson.auto
+import org.http4k.lens.Query
+import org.http4k.lens.nonEmptyString
 import org.http4k.routing.bind
 import org.http4k.routing.routes
-import org.http4k.format.Jackson.auto
-import org.http4k.format.Jackson.json
-import org.http4k.core.with
-import org.http4k.lens.nonEmptyString
+import org.http4k.server.Jetty
+import org.http4k.server.asServer
 
 data class Name(val name: String?)
 data class Greetings(val message: String)
 
 fun main(args: Array<String>) {
+    var queryNameLens = Query.nonEmptyString().defaulted("name", "world");
     val nameLens = Body.auto<Name>().toLens()
     val greetingsLens = Body.auto<Greetings>().toLens()
 
-    // val app = { request: Request -> Response(OK).body("Nightcode") }
-    val app = routes(
+    val endpoint = routes(
         "/" bind GET to { Response(OK).body("Nightcode") },
         "/api" bind routes(
             "/greetings" bind routes(
-                GET to { request: Request -> 
-                    val name: String? = request.query("name")
-                    Response(OK).with(greetingsLens of Greetings("hello, ${name ?: "world"}"))
+                GET to { request: Request ->
+                    val name: String = queryNameLens.extract(request)
+                    Response(OK).with(greetingsLens of Greetings("hello, ${name}"))
                 },
                 POST to { request: Request -> 
                     val name: Name = nameLens.extract(request)
@@ -41,6 +36,9 @@ fun main(args: Array<String>) {
             )
         )
     )
+
+    // Convert lenses conversion errors (500) into bad request (400)
+    val app = ServerFilters.CatchLensFailure.then(endpoint)
 
     app.asServer(Jetty(3000)).start()
 }
