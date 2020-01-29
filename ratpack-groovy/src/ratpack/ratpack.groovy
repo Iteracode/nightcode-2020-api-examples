bf07f83bd72c237c72e15694c02824999d47b7a6
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2020-01-29 23:11:25 +0100

    Ratpack + Groovy - Manage POST
    Very verbose at it, should be shorter with better Groovy code.

--- ratpack-groovy/src/ratpack/ratpack.groovy
+++ ratpack-groovy/src/ratpack/ratpack.groovy
@@ -1,19 +1,40 @@
 import static ratpack.groovy.Groovy.ratpack
 import static ratpack.jackson.Jackson.json
+import static ratpack.jackson.Jackson.fromJson
+
+class NameContainer {
+    String name
+}
 
 ratpack {
     handlers {
         get("") {
             render("Nightcode")
         }
-        get("api/greetings") { context ->
-            def name = request.queryParams.name != null
-                ? request.queryParams.name
-                : "world"
+        path("api/greetings") {
+            byMethod {
+                get { context ->
+                    def name = request.queryParams.name != null
+                        ? request.queryParams.name
+                        : "world"
 
-            name.isEmpty()
-                ? context.response.status(400).send()
-                : render(json([message: "hello, ${name}".toString()]))
+                    name.isEmpty()
+                        ? context.response.status(400).send()
+                        : render(json([message: "hello, ${name}".toString()]))
+                }
+                post { context ->
+                    parse(fromJson(NameContainer))
+                        .then { 
+                            def name = it.name != null
+                                ? it.name
+                                : "world"
+
+                        name.isEmpty()
+                            ? context.response.status(400).send()
+                            : render(json([message: "hello, ${name}".toString()]))
+                        }
+                }
+            }
         }
     }
 }
