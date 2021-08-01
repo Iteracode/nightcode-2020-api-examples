c81f5c37a49c50700aea41cdc0d5033feb290f46
Charles Fourdrignier <charles.fourdrignier@gmail.com>
2021-08-01 11:44:44 +0200

    Clean indentation and add stateless and banner options

--- racket/src/nightcode.rkt
+++ racket/src/nightcode.rkt
@@ -2,43 +2,43 @@
 
 (require racket/list
          racket/match
-	 web-server/http
+         web-server/http
          web-server/servlet-env
          web-server/dispatch
          web-server/http/bindings
-	 json)
+         json)
 
 (define (response
-	 #:code    [code/kw 200]
-	 #:message [message/kw "OK"]
-	 #:seconds [seconds/kw (current-seconds)]
-	 #:mime    [mime/kw #f]
-	 #:headers [headers/kw empty]
-	 #:body    [body/kw empty])
+         #:code    [code/kw 200]
+         #:message [message/kw "OK"]
+         #:seconds [seconds/kw (current-seconds)]
+         #:mime    [mime/kw #f]
+         #:headers [headers/kw empty]
+         #:body    [body/kw empty])
   (define mime
     (cond ((string? mime/kw)
-	   (string->bytes/utf-8 mime/kw))
+           (string->bytes/utf-8 mime/kw))
           ((bytes? mime/kw)
            mime/kw)
-	  (else
-	   #f)))
+          (else
+           #f)))
   (define message
 
     (cond ((bytes? message/kw)
-	   message/kw)
-	  ((string? message/kw)
-	   (string->bytes/utf-8 message/kw))
+           message/kw)
+          ((string? message/kw)
+           (string->bytes/utf-8 message/kw))
           (else
            #f)))
   (define body
     (cond ((string? body/kw)
-	   (list (string->bytes/utf-8 body/kw)))
-	  ((bytes? body/kw)
-	   (list body/kw))
-	  ((list? body/kw)
+           (list (string->bytes/utf-8 body/kw)))
+          ((bytes? body/kw)
+           (list body/kw))
+          ((list? body/kw)
            body/kw)
-	  (#t
-	   body/kw)))
+          (#t
+           body/kw)))
   (response/full
    code/kw
    message
@@ -49,7 +49,7 @@
 
 (define (not-found req)
   (response #:code 404
-	    #:message "Not Found"))
+            #:message "Not Found"))
 
 (define (bad-request)
   (response #:code 400
@@ -90,5 +90,7 @@
   (serve/servlet
    go
    #:port 6892
+   #:banner? #t
    #:command-line? #t
+   #:stateless? #t
    #:servlet-regexp #rx""))
